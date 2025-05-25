9351ebc29b7744e1a014cb221af3d62ef5e44ee8
JaydeepRamanuj <jaydeepramanuj.jd@gmail.com>
2025-05-25 21:46:48 +0530

    updated error and not-found page to avoid build issue

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -6,13 +6,6 @@ function ErrorPageRoot() {
   const router = useRouter();
   return (
     <div className="my-10 bg-black rounded-xl border border-yellow-500/20 shadow-xl shadow-yellow-500/5 p-6 flex flex-col justify-center items-center relative overflow-hidden text-center text-gray-200">
-      {/* <DotLottieReact
-        className="w-full max-w-xl mx-auto"
-        src="https://lottie.host/e3b2f5cf-da38-491d-ab01-19037e8c386e/hZKV4JwSDF.lottie"
-        loop
-        autoplay
-      /> */}
-
       <h1 className="text-2xl font-semibold text-yellow-400 mt-4">
         Something went wrong!
       </h1>
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,18 +1,10 @@
 "use client";
-// import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import React from "react";
 import { useRouter } from "next/navigation";
 function PageNotFoundRootLevel() {
   const router = useRouter();
   return (
     <div className="my-10 bg-black rounded-xl border border-yellow-500/20 shadow-xl shadow-yellow-500/5 p-6 flex flex-col justify-center items-center relative overflow-hidden text-center text-gray-200">
-      {/* <DotLottieReact
-        className="w-full max-w-xl mx-auto"
-        src="https://lottie.host/3e74a6c2-85f0-437c-8fe5-3450ca8cddc9/lo4KwWU9gy.lottie"
-        loop
-        autoplay
-      /> */}
-
       <h1 className="text-2xl font-semibold text-yellow-400 mt-4">
         Oops! Page not found.
       </h1>
